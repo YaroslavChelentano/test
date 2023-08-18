b55c556874fe347efae2b37f179b1ccd6f1dda1d
YaroslavChelentano <yaroslav.chelentano@gmail.com>
2023-08-18 18:12:13 +0300

    Add new project

---
+++ VariablesAndIf/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace VariablesAndIf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 5;
+            var name = "Oleg";
+            var damage = 50;
+            var isInHead = damage == 100;
+            if (isInHead)
+            {
+                Console.WriteLine("Head");
+            }
+            else
+            {
+                Console.WriteLine("Body");
+            }
+        }
+    }
+}
